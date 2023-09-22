f08f5b667c13104789eedba4abfe8538616a925c
Дмитрий Захаров <zds.vip1221@gmail.com>
2023-09-22 13:58:10 +0300

    gg

--- src/item.py
+++ src/item.py
@@ -13,7 +13,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +24,14 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
-    def apply_discount(self) -> None:
+    def apply_discount(self):
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        if self.pay_rate != 0.0 or self.pay_rate != 0:
+            self.price *= self.pay_rate
+            return self.price
+        else:
+            return self.price
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,25 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+a_test_1 = Item("Test1", 200, 5)
+a_test_2 = Item("Test2", 500, 2)
+a_test_3 = Item("Test3", 2, 0)
+
+b_test_1 = Item("Test1", 20000, 1)
+
+
+
+def test_answer_of_calculate_total_price():
+
+    assert a_test_1.calculate_total_price() == 1000
+    assert a_test_2.calculate_total_price() == 1000
+    assert a_test_3.calculate_total_price() == 0
+
+    Item.pay_rate = 0.8
+    assert b_test_1.apply_discount() == 16000
+
+def test_apply_discount():
+    Item.pay_rate = 1
+    assert b_test_1.apply_discount() == 20000
+
+
